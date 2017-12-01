1b5205d07a29cee68bc02d91f8000612f498c54a
Timothy Allen <flipper@peregrinesalon.com>
2017-12-01 13:47:15 -0500

    Add Python 3.6.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Framework :: Django',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
